23172e549491c88510096d8d8bc625126a8c1cc0
HejHej1041 <erik.lindstrom@elev.ga.ntig.se>
2023-03-25 17:18:01 +0100

    ss

--- mobile.js
+++ mobile.js
@@ -1,11 +1,9 @@
 const elemMobileMenu = document.querySelector(".mobile-menu")
-
-
-
-const elemPrimaryNavigation = document.querySelector(".subnav-content")
-const iconHamburger = document - querySelector(".icon-hamburger")
-const btnHamburger = document - querySelector(".btn-hamburger")
+const elemPrimaryNavigation = document.querySelector(".primary-navigation")
+const iconHamburger = document.querySelector(".icon-hamburger")
+const btnHamburger = document.querySelector(".btn-hamburger")
 
 btnHamburger.addEventListener("click", () => {
     elemPrimaryNavigation.classList.toggle("open");
 })
+
